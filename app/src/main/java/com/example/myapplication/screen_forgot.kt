fcf82f12432bd6cdba3252c2ccb4f8cb7b21bed2
thuanbudu112 <ledinhthuan27@gmail.com>
2024-11-21 13:11:54 +0700

    thuan bu lonn

--- app/src/main/java/com/example/myapplication/screen_forgot.kt
+++ app/src/main/java/com/example/myapplication/screen_forgot.kt
@@ -18,11 +18,6 @@ class screen_forgot : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val btn_back_forget = findViewById<Button>(R.id.btn_back_forget)
-        btn_back_forget.setOnClickListener{
-            val i = Intent(this, MainActivity::class.java)
-            startActivity(i)
-        }
         val btn_forgot_login = findViewById<Button>(R.id.btn_forgot_login)
         btn_forgot_login.setOnClickListener{
             val i = Intent(this, screen_Login::class.java)
